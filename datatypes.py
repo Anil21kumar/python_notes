9bba4f49e3f02974e7f56aa6b7e1f548149edfae
unknown <anilanilkummara123@gmail.com>
2025-06-11 17:42:36 +0530

    update the data

--- datatypes.py
+++ datatypes.py
@@ -1,4 +1,17 @@
-int
-float
-bool
+int 
+in this we can store numaric values ,it can contain singl degit or double or more it will consider as int
+a=1,
+b=12
+c=12345555555
+all are int 
+float 
+in this we can see the numbers inthis format
+a=10.00
+b=10.11
+a=10.01
+all like this are float
+bool 
+bool is refered for true or false only in python we call bool not boolion ok mr.anil yes sir
+    
 str
+string is refferd for charectors 
--- variable.py
+++ variable.py
@@ -3,3 +3,13 @@ a=30
 print(a)
 b= "cloud"
 print(b)
+
+   #variable=it is a unit that which store data,
+        # we can change the value in a variable as user wish
+        #a=10,
+# now we can enter new value into same variable like a=20 it will last assined value in the variable
+a=20 the value of a =20
+
+debug 
+
+it is a process that shows how the code is working in background
